64ee1f6b5c325c4f2e30c21a914083e48438ccc0
hannaarendt <hannaarendt02@hotmail.com>
2025-03-17 20:31:41 +0100

    add second text

---
+++ src/content/text.js
@@ -0,0 +1,5 @@
+export const text = {
+    short: "hello! my name is hanna and i like semlor, the color pink, taylor swift, horses, and jane austen. i'm currently studying my second year at LTH, a master of science in engineering in information and communication technologies.",
+    long: "my interest in programming started when i was 11 and created my first blog. i could sit for hours and hours editing the blog design to perfection (creating this website was really going back to my roots with HTML and CSS). since then, i've always know that it's what i wanted to do. there is few things that brings me the same amount of joy. :,)",
+    long2: "i especially adore the combination of the logical side of back end, together with the creative side of the front end."
+};
